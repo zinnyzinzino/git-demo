069e58a787ed62896abd1836bfb0fa0a4dc040b3
Saba Janelidze <janojanela@gmail.com>
2025-10-27 20:28:29 +0400

    bombarasclart

--- import math.py
+++ import math.py
@@ -1,21 +1,18 @@
 import math
-def solution(a,b,c):
-    #discriminant
-    d = b**2 - 4*a*c
-    if d < 0:  
-        print("No real roots")
-        return []
+
+def solution(a, b, c):
+    d = b**2 - 4 * a * c
+    
+    if d < 0:
+        return ()
+    
     elif d == 0:
-        x = -b / (2*a)
-        return [x]
+        x = -b / (2 * a)
+        return (float(x),)
+        
     else:
         sqrt_d = math.sqrt(d)
-        x1 = (-b + sqrt_d) / (2*a)
-        x2 = (-b - sqrt_d) / (2*a)
-        roots = tuple(sorted([x1, x2]))
-        return roots
-    
-    
-print(solution(1, -3, 2))
-print(solution(1, 4, 4))
-print(solution(1, 3, 45))
+        x1 = (-b - sqrt_d) / (2 * a)
+        x2 = (-b + sqrt_d) / (2 * a)
+        
+        return tuple(sorted((float(x1), float(x2))))
