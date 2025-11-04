2b9773eb47db71e0d4577ba49908e9e0ba288a1e
Saba Janelidze <janojanela@gmail.com>
2025-11-04 20:52:45 +0400

    FIX TSHI

--- import math.py
+++ import math.py
@@ -3,5 +3,3 @@ def some_expression_with_rounding(a, b):
     result = (12 * a + 25 * b) / (1 + a ** (2 ** b))
     return round(result, 2)
 
-print(some_expression_with_rounding(123, 1))
-print(some_expression_with_rounding(1.4, 2.55))
