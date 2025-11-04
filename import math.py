b13a3fa8d5fddfcc1a0ab1e44ad4199623c6bb89
Saba Janelidze <janojanela@gmail.com>
2025-11-04 20:25:49 +0400

    new task

--- import math.py
+++ import math.py
@@ -1,18 +1,7 @@
 import math
+def calculate_experssion(a,b):
+  result = (12*a + 25*b) / (1 + a**(2**b))
+  return round(result, 2)
 
-def solution(a, b, c):
-    d = b**2 - 4 * a * c
-    
-    if d < 0:
-        return ()
-    
-    elif d == 0:
-        x = -b / (2 * a)
-        return (float(x),)
-        
-    else:
-        sqrt_d = math.sqrt(d)
-        x1 = (-b - sqrt_d) / (2 * a)
-        x2 = (-b + sqrt_d) / (2 * a)
-        
-        return tuple(sorted((float(x1), float(x2))))
+
+print(calculate_experssion(2,3))
