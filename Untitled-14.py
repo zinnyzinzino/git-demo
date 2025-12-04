8a2dfaa2de3f02e00bdda7a7bcfc487ec3c82838
Saba Janelidze <janojanela@gmail.com>
2025-12-04 23:45:48 +0400

    GOOOOOOOOOOOad

--- Untitled-14.py
+++ Untitled-14.py
@@ -1,22 +1,41 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-def printlist(head):
-    current = head
-    while current:
-        print(current.data, end = "->")
-        current = current.next
-        print("null")
+class Stack:
+    def __init__(self, cap):
+        self.arr = [0] * cap
+        self.capacity = cap
+        self.top = -1
 
-node1 = Node(1)
-node2 = Node(4)
-node3 = Node(2)
-node4 = Node(7)
+    def push(self, x):
+        if self.top == self.capacity - 1:
+            raise OverflowError("overflow")
+        self.top += 1
+        self.arr[self.top] = x
+
+    def pop(self):
+        if self.top == -1:
+            raise IndexError("underflow")
+        x = self.arr[self.top]
+        self.top -= 1
+        return x
+
+    def peek(self):
+        if self.top == -1:
+            raise IndexError("empty")
+        return self.arr[self.top]
+
+    def isempty(self):
+        return self.top == -1
+
+    def size(self):
+        return self.top + 1
 
 
-node1.next = node2
-node2.next = node3
-node3.next = node4
+if __name__ == "__main__":
+    st = Stack(10)
+    st.push(1)
+    st.push(3)
+    st.push(4)
+    st.push(2)
 
-printlist(node1)
+    print("popped:", st.pop())
+    print("top:", st.peek())
+    print("size:", st.size())
---
+++ Untitled-3.py
@@ -0,0 +1,22 @@
+class DYnamicstack:
+    def __init__(self):
+        self.items = []
+        
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if self.is_empty():
+            raise IndexError("underflow")
+        return self.items.pop()
+    
+    def peek(self):
+        if self.is_empty():
+            raise IndexError("empty")
+        return self.items[-1]
+    
+    def is_empty(self):
+        return len(self.items) == 0 
+    
+    def size(self):
+        return len(self.items)
