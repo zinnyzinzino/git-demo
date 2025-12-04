f3f23b9a53dd7426dec8e5fd370ec47cea1e5192
Saba Janelidze <janojanela@gmail.com>
2025-12-04 20:37:34 +0400

    yipyip

--- Untitled-2.py
+++ Untitled-2.py
@@ -2,21 +2,25 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
-def printlist(head):
-    current = head
-    while current:
-        print(current.data, end = "->")
-        current = current.next
-        print("null")
-
-node1 = Node(1)
-node2 = Node(4)
-node3 = Node(2)
-node4 = Node(7)
-
+    @staticmethod
+    def findlowest(head):
+        minvalue = head.data
+        currentnode = head.next
+        while currentnode:
+            if currentnode.data < minvalue:
+                minvalue = currentnode.data
+            currentnode = currentnode.next
+        return minvalue
+    
+node1 = Node(7)
+node2 = Node(9)
+node3 = Node(15)
+node4 = Node(6)
+node5 = Node(0)
 
 node1.next = node2
 node2.next = node3
 node3.next = node4
+node4.next = node5
 
-printlist(node1)
+print (f"lowest value: {Node.findlowest(node1)}")
